9576ab0620b79dd70759754be54efd2ded9c161f
NaorBD <naorbend@gmail.com>
2023-12-15 18:43:33 +0200

    continued menu, added print calculator

--- main.py
+++ main.py
@@ -1,48 +1,77 @@
-def menu():
+import os
+
+
+# actions functions
+def sumNums(num1, num2):
+    return num1 + num2
+
+
+def subNums(num1, num2):
+    return num1 - num2
+
+
+def mulNums(num1, num2):
+    return num1 * num2
+
+
+def divNums(num1, num2):
+    return num1 / num2
+
+
+def toPowerNums(num1, num2):
+    return num1 ** num2
+
+
+# print calculator configuration
+def printCalculator():
+    os.system('cls')
     print("\n")
     print("          calculator")
     print("-------------------------------")
     print("\n")
+
+
+# menu function
+def menu():
+    operate = ''
+    num1 = 0
+    num2 = 0
+    printCalculator()
     num1 = input("num1:                ")
+    num1 = int(num1)
     print("actions: 1.   add   2.  sub    3. mul ")
     print("         4.   div   5.  pow    6. end ")
-    action = input("input")
-    if(int(action) == 1):
-        operator = '+'
-    if(int(action) == 2):
-        operator = '-'
-    if (int(action) == 3):
-        operator = '*'
-    if (int(action) == 4):
-        operator = '/'
-    if (int(action) == 5):
-        operator = '^'
-    if(int(action) == 6):
+    action = input("input:")
+    if int(action) == 1:
+        operate = '+'
+    if int(action) == 2:
+        operate = '-'
+    if int(action) == 3:
+        operate = '*'
+    if int(action) == 4:
+        operate = '/'
+    if int(action) == 5:
+        operate = '^'
+    if int(action) == 6:
         return
-    print("\n")
-    print(num1+operator)
+    printCalculator()
+    print(str(num1)+operate)
     num2 = input("num2:                ")
-    print("\n")
-    print(num1+operator+num2)
-    if (int(action) == 1):
+    num2 = int(num2)
+    printCalculator()
+    print(str(num1)+operate+str(num2))
+    if int(action) == 1:
         sumNums(num1, num2)
-    if (int(action) == 2):
+    if int(action) == 2:
         subNums(num1, num2)
-    if (int(action) == 3):
+    if int(action) == 3:
         mulNums(num1, num2)
-    if (int(action) == 4):
+    if int(action) == 4:
         divNums(num1, num2)
-    if (int(action) == 5):
+    if int(action) == 5:
         toPowerNums(num1, num2)
-    if (int(action) == 6):
+    if int(action) == 6:
         return
-def sumNums(num1, num2):
-    return (num1 + num2)
-def subNums(num1, num2):
-    return (num1 - num2)
-def mulNums(num1, num2):
-    return (num1 * num2)
-def divNums(num1, num2):
-    return (num1 / num2)
-def toPowerNums(num1, num2):
-    return (num1 ** num2)
+
+
+menu()
